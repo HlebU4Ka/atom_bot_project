f00c6341b817d85ffa796de73a29a49d5757b185
glebk <glebkot57@gmail.com>
2023-11-14 20:46:22 +0200

    Настройки celery

--- settings_app/settings.py
+++ settings_app/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ya%3p+=$kh1o@(ghww%7b68t1mgf7xf1w25*nnlrd#@6y$k5j'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-9ya%3p+=$kh1o@(ghww%7b68t1mgf7xf1w25*nnlrd#@6y$k5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
