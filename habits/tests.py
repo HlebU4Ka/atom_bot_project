76851e291cac2acfe4a5e103197017fa5f554996
glebk <glebkot57@gmail.com>
2023-11-14 22:40:55 +0200

    Откат. Поправил celery.py. Добавил tasks

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,31 @@
+import pytest
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from models import Habit
 
-# Create your tests here.
+User = get_user_model()
+
+@pytest.mark.django_db
+class TestHabitModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_create_habit(self):
+        habit = Habit.objects.create(user=self.user, name='Exercise', frequency='daily')
+        self.assertEqual(habit.name, 'Exercise')
+        self.assertEqual(habit.frequency, 'daily')
+
+
+    def test_update_habit(self):
+        habit = Habit.objects.create(user=self.user, name='Exercise', frequency='daily')
+        habit.name = 'New Exercise'
+        habit.save()
+        updated_habit = Habit.objects.get(id=habit.id)
+        self.assertEqual(updated_habit.name, 'New Exercise')
+
+    def test_delete_habit(self):
+        habit = Habit.objects.create(user=self.user, name='Exercise', frequency='daily')
+        habit_id = habit.id
+        habit.delete()
+        with self.assertRaises(Habit.DoesNotExist):
+            Habit.objects.get(id=habit_id)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from settings_app.tasks import schedule_habit_notification
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TestScheduleHabitNotification(TestCase):
+    def test_schedule_habit_notification(self, expected_value=None):
+        # Создайте тестового пользователя и запустите задачу
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        schedule_habit_notification.delay(user_id=user.id, habit_id=1)  # Пример параметров
+
+        # Проверьте, что задача выполнена успешно
+        # Важно: в реальном приложении вы, возможно, захотите использовать более подходящие методы проверки
+        # в зависимости от того, как вы реализуете отправку сообщений в вашем коде.
+        # Например, можно проверить, что в базе данных создана запись об отправленном сообщении.
+        self.assertEqual(user.some_expected_value, expected_value, "Ожидаемый результат после выполнения задачи")
